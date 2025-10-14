09e7b010214f765a071c1c7bf91e2faff9bf5bc9
Kaio <74631438+kaionabuco@users.noreply.github.com>
2025-10-13 21:45:35 -0300

    fix: login redireciona para biblioteca
    Navegação programática mantida em detrimento da navegação por link, também no cadastro, para manter o comportamento dos forms mais consistente.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -118,9 +118,7 @@ export default function Register() {
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
-                <Link to="/home">
                     Registrar
-                </Link>
               </button>
             </div>
           </form>
