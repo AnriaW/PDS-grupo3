5c1a4ee0ab3ce165e7741a261333b7179efe8105
Kaio <74631438+kaionabuco@users.noreply.github.com>
2025-10-20 14:32:38 -0300

    refactor: hooks/cookies.jsx deprecado e deletado

--- src/hooks/cookies.jsx
+++
@@ -1,5 +0,0 @@
-// hooks/cookies.js
-export function getCookie(name) {
-    const match = document.cookie.match(new RegExp('(?:^|; )' + name.replace(/[$()*+./?[\\\]^{|}-]/g, '\\$&') + '=([^;]*)'));
-    return match ? match[1] : null;
-  }
